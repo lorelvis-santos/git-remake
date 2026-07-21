1195557553bda4c823609b75a34b80e69879157b
Lorelvis Santos <santoslorelvis@gmail.com>
2026-07-21 07:29:23 -0400

    refactor(main): update GitBlob initialization and API consumption
    Migrate from raw pointers and C-style I/O to C++ standard streams and std::string_view for blob testing.

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,37 @@
 #include "GitBlob.h"
 #include <cstdio>
 #include <cstdlib>
+#include <iostream>
 
 int main() {
-  GitBlob blob("test.txt");
+  std::string_view path = "src/main.cpp";
+  GitBlob blob(path);
 
   if (!blob.is_valid()) {
-    printf("El blob no es válido\n");
+    std::cerr << "El blob no es válido" << std::endl;
     return 1;
   }
 
-  printf("Tamaño del blob en bytes: %lu\n", blob.get_size());
-  printf("Contenido del blob:\n");
+  std::cout << "Blob de " << path << ":\n" << std::string(blob.get_data().begin(), blob.get_data().end()) << std::endl;
+  std::cout << "Hash de " << path << ":\n" << blob.get_hash() << std::endl;
 
-  const char* data = blob.get_data();
+  // printf("Tamaño del blob en bytes: %lu\n", blob.get_size());
+  // printf("Contenido del blob:\n");
 
-  for (size_t i = 0; i < blob.get_size(); i++) {
-    if (data[i] == '\0') {
-      printf("\\0");
-    } else {
-      printf("%c", data[i]);
-    }
-  }
-  printf("\n");
+  // const char* data = blob.get_data();
 
-  char* hash = (char*)malloc(65);
-  blob.get_hash(hash);
-  printf("hash: %s\n", hash);
+  // for (size_t i = 0; i < blob.get_size(); i++) {
+  //   if (data[i] == '\0') {
+  //     printf("\\0");
+  //   } else {
+  //     printf("%c", data[i]);
+  //   }
+  // }
+  // printf("\n");
+
+  // char* hash = (char*)malloc(65);
+  // blob.get_hash(hash);
+  // printf("hash: %s\n", hash);
 
   return 0;
 }
