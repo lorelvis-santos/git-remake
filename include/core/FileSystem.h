bf2140d44a7aa3620035c6927e606e0a3f9d178a
Lorelvis Santos <santoslorelvis@gmail.com>
2026-07-24 09:46:31 -0400

    feat(core): add object reading capabilities to FileSystem
    - Implement read_object function to locate and read binary blob files from the .gitrk/objects directory using their hash.
    - Refactor read_file to support std::filesystem::path natively.
    - Introduce a centralized constant for the objects path.

--- include/core/FileSystem.h
+++ include/core/FileSystem.h
@@ -4,8 +4,13 @@
 #include <cstdint>
 #include <string>
 #include <optional>
+#include <filesystem>
+
+namespace fs = std::filesystem;
 
 namespace FileSystem {
   std::optional<std::vector<uint8_t>> read_file(const std::string_view path);
+  std::optional<std::vector<uint8_t>> read_file(const fs::path path);
+  std::optional<std::vector<uint8_t>> read_object(const std::string& hash);
   bool write_object(const std::string& hash, const std::vector<uint8_t>& bytes);
 }
--- src/core/FileSystem.cpp
+++ src/core/FileSystem.cpp
@@ -7,18 +7,24 @@
 #include <fstream>
 #include <cstdint>
 
+#define OBJECTS_PATH ".gitrk/objects"
+
 namespace fs = std::filesystem;
 
 namespace FileSystem {
   // Función para leer los bytes de un archivo
   std::optional<std::vector<uint8_t>> read_file(const std::string_view path) {
+    return read_file(fs::path(path));
+  }
+
+  std::optional<std::vector<uint8_t>> read_file(const fs::path path) {
     // std::ios::ate pone el cursor al final del archivo
     // la idea es contar los bytes que hay para reservar el espacio del vector
     // y luego poder leerlo todo
 
     // hay que pasar path como std::string(path) porque el string_view no 
     // asegura que el string termine con el byte nulo
-    std::ifstream file(std::string(path), std::ios::binary | std::ios::ate);
+    std::ifstream file(path, std::ios::binary | std::ios::ate);
 
     if (!file.is_open()) {
       return std::nullopt;
@@ -35,12 +41,30 @@ namespace FileSystem {
     return buffer;
   }
 
+  std::optional<std::vector<uint8_t>> read_object(const std::string& hash) {
+    fs::path dir(OBJECTS_PATH);
+
+    // separar las primeras dos letras del resto del hash.
+    std::string hash_start = hash.substr(0, 2);
+    std::string file_name = hash.substr(2);
+
+    // construir paths del object
+    fs::path dir_path(dir / hash_start);
+    fs::path file_path(dir_path / file_name);
+
+    if (!fs::exists(dir_path)) {
+      return std::nullopt;
+    }
+
+    return read_file(file_path);
+  }
+
   // Función para persistir un blob/object
   bool write_object(const std::string& hash, const std::vector<uint8_t>& bytes) {
     // Pasos para escribir el object
     
     // path base, esto hay que refactorizarlo
-    fs::path dir(".gitrk/objects");
+    fs::path dir(OBJECTS_PATH);
 
     // separar las primeras dos letras del resto del hash.
     std::string hash_start = hash.substr(0, 2);
