44118bb91fc21f157c0182fd00657b5cbd344ddf
Lorelvis Santos <santoslorelvis@gmail.com>
2026-07-25 08:58:03 -0400

    feat(core): support partial hash matching for object lookups
    - Introduce get_object_path to resolve objects using short hashes (minimum 4 characters).
    - Add helper functions (build_object_dir, build_object_path) to streamline path generation.
    - Add validation to ensure hashes are at least 4 characters long before read/write operations.

--- include/core/FileSystem.h
+++ include/core/FileSystem.h
@@ -10,6 +10,13 @@ namespace fs = std::filesystem;
 
 namespace FileSystem {
   std::optional<std::vector<uint8_t>> read_file(const fs::path& path);
+
+  fs::path build_object_dir(const std::string& hash);
+  fs::path build_object_path(const std::string& hash);
+  
+  std::string get_object_name(const std::string& hash);
+  std::optional<fs::path> get_object_path(const std::string& hash);
+
   std::optional<std::vector<uint8_t>> read_object(const std::string& hash);
   bool write_object(const std::string& hash, const std::vector<uint8_t>& bytes);
 }
--- src/core/FileSystem.cpp
+++ src/core/FileSystem.cpp
@@ -1,4 +1,5 @@
 #include "core/FileSystem.h"
+#include "core/StringUtils.h"
 #include <fstream>
 #include <vector>
 #include <iostream>
@@ -10,8 +11,25 @@
 #define OBJECTS_PATH ".gitrk/objects"
 
 namespace fs = std::filesystem;
+namespace str = StringUtils;
 
 namespace FileSystem {
+  fs::path build_object_dir(const std::string& hash) {
+    fs::path objects_dir(OBJECTS_PATH);
+    std::string hash_start = hash.substr(0, 2);
+    return fs::path(objects_dir / hash_start);
+  }
+
+  fs::path build_object_path(const std::string& hash) {
+    fs::path object_dir = build_object_dir(hash);
+    std::string hash_rest = hash.substr(2);
+    return fs::path(object_dir / hash_rest);
+  }
+
+  std::string get_object_name(const std::string& hash) {
+    return hash.substr(2);
+  }
+
   // Función para leer los bytes de un archivo
   std::optional<std::vector<uint8_t>> read_file(const fs::path& path) {
     // std::ios::ate pone el cursor al final del archivo
@@ -37,50 +55,77 @@ namespace FileSystem {
     return buffer;
   }
 
-  std::optional<std::vector<uint8_t>> read_object(const std::string& hash) {
-    fs::path dir(OBJECTS_PATH);
+  std::optional<fs::path> get_object_path(const std::string& hash) {
+    // Verificamos que el hash sea minimo de 4 caracteres
+    if (hash.size() < 4) {
+      return std::nullopt;
+    }
+  
+    fs::path object_dir = build_object_dir(hash);
 
-    // separar las primeras dos letras del resto del hash.
-    std::string hash_start = hash.substr(0, 2);
-    std::string file_name = hash.substr(2);
-
-    // construir paths del object
-    fs::path dir_path(dir / hash_start);
-    fs::path file_path(dir_path / file_name);
-
-    if (!fs::exists(dir_path)) {
+    // Verificamos que el dir exista
+    if (!fs::exists(object_dir) || !fs::is_directory(object_dir)) {
       return std::nullopt;
     }
 
-    return read_file(file_path);
+    // Buscamos dentro del dir con el resto de la cadena del hash
+    bool found = false;
+    fs::path object_name;
+
+    for (const auto& entry : fs::directory_iterator(object_dir)) {
+      if (!entry.is_regular_file() || !str::starts_with(entry.path().filename().c_str(), get_object_name(hash))) {
+        continue;
+      }
+
+      if (found) {
+        return std::nullopt;
+      }
+
+      object_name = entry.path().filename();
+      found = true;
+    }
+
+    if (!found) {
+      return std::nullopt;
+    }
+
+    return fs::path(object_dir / object_name);
+  }
+
+  std::optional<std::vector<uint8_t>> read_object(const std::string& hash) {
+    if (hash.size() < 4) {
+      return std::nullopt;
+    }
+
+    std::optional<fs::path> object_path = get_object_path(hash);
+
+    if (!object_path) {
+      return std::nullopt;
+    }
+
+    return read_file(object_path.value());
   }
 
   // Función para persistir un blob/object
   bool write_object(const std::string& hash, const std::vector<uint8_t>& bytes) {
-    // Pasos para escribir el object
-    
-    // path base, esto hay que refactorizarlo
-    fs::path dir(OBJECTS_PATH);
+    if (hash.size() < 4) {
+      return false;
+    }
 
-    // separar las primeras dos letras del resto del hash.
-    std::string hash_start = hash.substr(0, 2);
-    std::string file_name = hash.substr(2);
-
-    // construir paths del object
-    fs::path dir_path(dir / hash_start);
-    fs::path file_path(dir_path / file_name);
+    fs::path object_dir = build_object_dir(hash);
+    fs::path object_path = build_object_path(hash);
 
     // comprobar que la ruta del hash exista
     // si no, se crea
-    if (!fs::exists(dir_path)) {
-      fs::create_directories(dir_path);
+    if (!fs::exists(object_dir)) {
+      fs::create_directories(object_dir);
     }
 
     // escribir el archivo con el resto del hash y con el contenido de bytes
-    std::ofstream file(file_path, std::ios::out | std::ios::binary);
+    std::ofstream file(object_path , std::ios::out | std::ios::binary);
 
     if (!file.is_open()) {
-      std::cerr << "No se pudo crear el archivo en la ruta " << file_path;
+      std::cerr << "No se pudo crear el archivo en la ruta " << object_path ;
       return false;
     }
 
