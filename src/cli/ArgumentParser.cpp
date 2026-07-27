f29dc55c0dd15dce198d7bc65c2f7df58665647a
Lorelvis Santos <santoslorelvis@gmail.com>
2026-07-27 00:40:14 -0400

    feat(cli): implement command-line argument parsing logic
    - Add logic to distinguish between operands, flags (short and long), and options (key=value).
    - Use StringUtils to cleanly parse and categorize arguments into the CommandArguments struct.

--- src/cli/ArgumentParser.cpp
+++ src/cli/ArgumentParser.cpp
@@ -1,12 +1,35 @@
 #include <cli/ArgumentParser.h>
+#include <core/StringUtils.h>
 #include <iostream>
+
+namespace str = StringUtils;
 
 namespace ArgumentParser {
   CommandArguments parse(const std::vector<std::string_view>& raw_args) {
     CommandArguments parsed_args = {};
-
+    
     for (auto arg : raw_args) {
-      std::cout << arg << "\n";
+      if (!str::starts_with(arg, "-")) {
+        parsed_args.operands.push_back(arg);
+        continue;
+      }
+
+      std::string_view token = arg;
+
+      if (token.size() >= 2 && token.substr(0, 2) == "--") {
+        token.remove_prefix(2);
+      } else if (token.size() >= 1 && token[0] == '-') {
+        token.remove_prefix(1);
+      }
+
+      auto separator_index = token.find("=");
+      if (separator_index != std::string::npos) {
+        parsed_args.options[token.substr(0, separator_index)] = token.substr(separator_index + 1);
+        continue;
+      }
+
+      // parametro por defecto -p, --parametro
+      parsed_args.flags.insert(token);
     }
 
     return parsed_args;
